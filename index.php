226ce7a0e6f9da48c0b036b37681dabbebc90334
Vishnu <vishnu@BuildServer.(none)>
2013-12-26 12:47:02 -0500

    Barebone code to list the structure of a given directory

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-	echo "Hello World";
+$dir = '../';
+displayStructure($dir);
+function displayStructure($dir) {
+	$files = scandir($dir);
+	foreach($files as $file) {
+		echo "<a href='".$dir.$file."'>".$file."</a><br>";
+	}
+}
 ?>

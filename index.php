ed25d0f4f837c05fb69edad624c51236c805e390
Vishnu <vishnu@BuildServer.(none)>
2013-12-26 13:00:32 -0500

    Add code to display login box

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 $dir = '../';
+displayLoginBox();
 displayStructure($dir);
+
+function displayLoginBox() {
+	echo <<<'HTML'
+	<form>
+		Username : <input type="text" name="username"><br>
+		Password : <input type="password" name="password"><br>
+		<input type="submit" value="Submit">
+	</form>;
+HTML;
+}
+
 function displayStructure($dir) {
 	$files = scandir($dir);
 	foreach($files as $file) {
